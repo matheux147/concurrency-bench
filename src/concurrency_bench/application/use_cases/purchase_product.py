9117990fefc6a440aa95269a5fb56aafef6240fc
Matheus Rennan <matheusrennan70@gmail.com>
2026-08-15 17:17:33 -0300

    feat: criar caso de uso purchase_product e exemplo de race condition

---
+++ src/concurrency_bench/application/use_cases/purchase_product.py
@@ -0,0 +1,14 @@
+from uuid import UUID
+
+from concurrency_lab.application.ports import ProductRepository
+
+
+class PurchaseProduct:
+    """Orquestra uma compra individual sem conhecer a infraestrutura."""
+
+    def __init__(self, repository: ProductRepository, product_id: UUID) -> None:
+        self._repository = repository
+        self._product_id = product_id
+
+    def execute(self) -> bool:
+        return self._repository.purchase(self._product_id)
